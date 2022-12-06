7e6f53a21fa114dc99d3756d748025e10de37d25
Prajjwal Veer Basnet <prajjwalveerbasnet10@gmail.com>
2022-12-06 23:59:59 +0545

    Insertion Sort and Bubble Sort

---
+++ Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Bubble_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] unsortedList = { 36, 2, 29, 1, 8, 14};
+            Console.WriteLine("Unsorted List : " + String.Join(",", unsortedList));
+            double[] sortedList = BubbleSort(unsortedList);
+            Console.WriteLine("Sorted List : " + String.Join(",",sortedList));
+
+
+        }
+        public static double[] BubbleSort(double[] unsortedList)
+        {
+            double temp;
+
+            for (int i = 0; i < unsortedList.Length-1; i++)
+            {
+                for(int j = 0; j < unsortedList.Length -(1+i); j++)
+                {
+                    if(unsortedList[j] > unsortedList[j + 1])
+                    {
+                        temp = unsortedList[j+1];
+                        unsortedList[j+1] = unsortedList[j];
+                        unsortedList[j] = temp;
+                    }
+                }
+            }
+            return unsortedList;
+        }
+    }
+}
---
+++ Insertion Sort/Insertion Sort/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Insertion_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] unsortedListBest = { 4, 6, 7, 8, 10};
+            double[] unsortedListAverage = { 10, 4, 8, 6, 7};
+            double[] unsortedListWorst = { 10, 8, 7, 6, 4 };
+
+            Console.WriteLine("Unsorted List : " + String.Join(", ", unsortedListWorst));
+            double[] sortedList = InsertionSort(unsortedListWorst);
+            Console.WriteLine("Sorted List : " + String.Join(",", sortedList));
+
+
+        }
+        private static double[] InsertionSort(double[] unsortedList)
+        {
+            int i = 1;
+            int j = i;
+            double temp = 0;
+
+            while(i < unsortedList.Length)
+            {
+                j = i;
+
+                while(j > 0 && unsortedList[j -1] > unsortedList[j])
+                {
+                    temp = unsortedList[j];
+                    unsortedList[j] = unsortedList[j - 1];
+                    unsortedList[j - 1] = temp;
+                    j--;    
+                }
+                i++;
+            }
+            return unsortedList;
+        }
+    }
+}
